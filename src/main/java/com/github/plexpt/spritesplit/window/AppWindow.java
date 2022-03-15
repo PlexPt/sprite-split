b2228c55ad32f2d35df14ded9a905b8a3bc39df5
plexpt <plexpt@gmail.com>
2022-03-15 20:32:56 +0800

    TransForm

--- src/main/java/com/github/plexpt/spritesplit/window/AppWindow.java
+++ src/main/java/com/github/plexpt/spritesplit/window/AppWindow.java
@@ -19,14 +19,18 @@ import lombok.Getter;
 @Getter
 public class AppWindow {
 
-    private static final GridConstraints GRID_CONSTRAINTS = new GridConstraints(0, 0, 1, 1, GridConstraints.ANCHOR_CENTER, GridConstraints.FILL_BOTH, GridConstraints.SIZEPOLICY_CAN_SHRINK | GridConstraints.SIZEPOLICY_CAN_GROW, GridConstraints.SIZEPOLICY_CAN_SHRINK | GridConstraints.SIZEPOLICY_CAN_GROW, null, new Dimension(200, 200), null, 0, false);
+    private static final GridConstraints GRID_CONSTRAINTS = new GridConstraints(0, 0, 1, 1,
+            GridConstraints.ANCHOR_CENTER, GridConstraints.FILL_BOTH,
+            GridConstraints.SIZEPOLICY_CAN_SHRINK | GridConstraints.SIZEPOLICY_CAN_GROW,
+            GridConstraints.SIZEPOLICY_CAN_SHRINK | GridConstraints.SIZEPOLICY_CAN_GROW, null,
+            new Dimension(200, 200), null, 0, false);
 
     private JPanel mainPanel;
     private JTabbedPane tabbedPane;
     private JPanel aboutPanel;
     private JPanel spritecutPanel;
     private JPanel resizePanel;
-    private JPanel infinityPanel;
+    private JPanel transPanel;
     private JPanel boostPanel;
     private JPanel schedulePanel;
     private JPanel settingPanel;
@@ -59,18 +63,24 @@ public class AppWindow {
     public void init() {
         window = getInstance();
         window.getMainPanel().updateUI();
-        window.getSpritecutPanel().add(SpriteCutPannel.getInstance().getContent(), GRID_CONSTRAINTS);
+        window.getSpritecutPanel().add(SpriteCutPannel.getInstance().getContent(),
+                GRID_CONSTRAINTS);
         window.getResizePanel().add(ResizeForm.getInstance().getContent(), GRID_CONSTRAINTS);
         window.getAboutPanel().add(AboutForm.getInstance().getAboutPanel(), GRID_CONSTRAINTS);
-//        window.getSchedulePanel().add(ScheduleForm.getInstance().getSchedulePanel(), GRID_CONSTRAINTS);
-//        window.getPushHisPanel().add(PushHisForm.getInstance().getPushHisPanel(), GRID_CONSTRAINTS);
-//        window.getSettingPanel().add(SettingForm.getInstance().getSettingPanel(), GRID_CONSTRAINTS);
-//        window.getMessageEditPanel().add(MessageEditForm.getInstance().getMessageEditPanel(), GRID_CONSTRAINTS);
-//        window.getMessageManagePanel().add(MessageManageForm.getInstance().getMessageManagePanel(), GRID_CONSTRAINTS);
+        window.getTransPanel().add(TransForm.getInstance().getContent(), GRID_CONSTRAINTS);
+//        window.getPushHisPanel().add(PushHisForm.getInstance().getPushHisPanel(),
+//        GRID_CONSTRAINTS);
+//        window.getSettingPanel().add(SettingForm.getInstance().getSettingPanel(),
+//        GRID_CONSTRAINTS);
+//        window.getMessageEditPanel().add(MessageEditForm.getInstance().getMessageEditPanel(),
+//        GRID_CONSTRAINTS);
+//        window.getMessageManagePanel().add(MessageManageForm.getInstance()
+//        .getMessageManagePanel(), GRID_CONSTRAINTS);
 //        window.getPushPanel().add(PushForm.getInstance().getPushPanel(), GRID_CONSTRAINTS);
-//        window.getSpritecutPanel().add(MessageTypeForm.getInstance().getMessageTypePanel(), GRID_CONSTRAINTS);
+//        window.getSpritecutPanel().add(MessageTypeForm.getInstance().getMessageTypePanel(),
+//        GRID_CONSTRAINTS);
 //        window.getBoostPanel().add(BoostForm.getInstance().getBoostPanel(), GRID_CONSTRAINTS);
-//        window.getInfinityPanel().add(InfinityForm.getInstance().getInfinityPanel(), GRID_CONSTRAINTS);
+//        window.getTransPanel().add(InfinityForm.getInstance().getTransPanel(), GRID_CONSTRAINTS);
 //        window.getMessagePanel().setDividerLocation((int) (App.mainFrame.getWidth() / 5.6));
         window.getMainPanel().updateUI();
     }
